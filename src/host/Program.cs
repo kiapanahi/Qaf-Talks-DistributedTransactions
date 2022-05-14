f34df0479358ed2b707ea79fa15d9b9d68e807b9
Kia Panahi Rad <kia.panahirad@gmail.com>
2022-05-14 14:31:58 +0430

    Basic MediatR setup

--- src/host/Program.cs
+++ src/host/Program.cs
@@ -1,4 +1,12 @@
+using System.Reflection;
+using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddMediatR(Assembly.GetExecutingAssembly());
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
